30ce517bdcfe00356107bd52cc9f5503c9743a8d
vinodhp <vinodhpolamarasetty@gmail.com>
2020-04-10 10:55:23 +0530

    cookie changes

--- stocks/views.py
+++ stocks/views.py
@@ -28,7 +28,8 @@ class LoginView(APIView):
         user = serializer.validated_data["user"]
         django_login(request,user)
         token, created = Token.objects.get_or_create(user=user)
-        return Response({"token":token.key},status=200)
+        req_obj = request.META.get("HTTP_COOKIE",request.META.get('CSRF_COOKIE',"")))
+        return Response({"token":token.key,"csrf":req_obj},status=200)
 
     
 
