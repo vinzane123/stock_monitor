c4e44a17774ccff1914c06bebcf8d46890058e73
vinodhp <vinodhpolamarasetty@gmail.com>
2020-04-12 07:34:20 +0530

    azurechanges

--- stock_monitor/settings.py
+++ stock_monitor/settings.py
@@ -53,7 +53,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
     "https://stocksmonitor.heroku.com",
     "http://localhost:8000",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://stocksmonitor.azurewebsites.net"
 ]
 
 ROOT_URLCONF = 'stock_monitor.urls'
